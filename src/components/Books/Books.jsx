a8f67ab2fe2354dfa2e29ca57e019e27752ef46a
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-02-05 19:51:32 +0600

    added books data fetching api

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 
 
 const Books = () => {
+    const [books,setBooks]=useState([]);
+    useEffect(()=>{
+        fetch('./booksData.json')
+        .then(res=>res.json())
+        .then(data=>setBooks(data))
+    },[])
     return (
         <div>
             <h2 className="teext-4xl font-bold text-center">Books</h2>
+            <p>{books.length}</p>
         </div>
     );
 };
