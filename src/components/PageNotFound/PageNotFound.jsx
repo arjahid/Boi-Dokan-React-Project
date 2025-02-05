97f9dc11138389cf2f4e25e48438f193ab2a532a
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-02-05 19:29:38 +0600

    added basic tailwindcss,daisyui,react-router,added basic path,and navbar

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,11 @@
+
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">Page not found</h2>
+        </div>
+    );
+};
+
+export default PageNotFound;
---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import{
+  createBrowserRouter,
+  RouterProvider,
+}from 'react-router-dom'
+import Root from './components/Root/Root'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Home from './components/Home/Home'
+import Dashboard from './components/Dashboard/Dashboard'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root></Root>,
+    errorElement:<PageNotFound></PageNotFound>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'dashboard',
+        element:<Dashboard></Dashboard>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
